98bcaed17c0dc56658d6cc21adc4573c57f8b941
evertzner <esteban.vertzner@gmail.com>
2022-06-09 16:17:55 +0200

    dark light mode theme

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,24 +1,40 @@
 import { Outlet, useNavigate } from "react-router-dom";
 
+import { useDispatch, useSelector } from "react-redux";
+
+import { uiActions } from "../../store/ui-slice";
+
 import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const theme = useSelector((state) => state.ui.theme);
 
   const onTitleClickHandler = () => {
     navigate("/");
   };
+
+  const onChangeThemeHandler = () => {
+    dispatch(uiActions.toggleTheme());
+    document.body.setAttribute("data-theme", theme);
+  };
+
   return (
     <div className="navigation">
       <div className="header">
         <div className="header__title" onClick={onTitleClickHandler}>
           Where in the world?
         </div>
-        <div className="header__dark-mode">
+        <div className="header__dark-mode" onClick={onChangeThemeHandler}>
           <span>
-            <ion-icon name="moon-outline"></ion-icon>
+            {theme === "light" ? (
+              <ion-icon name="sunny"></ion-icon>
+            ) : (
+              <ion-icon name="moon"></ion-icon>
+            )}
           </span>
-          Dark Mode
+          {theme === "light" ? "Light Mode" : "Dark Mode"}
         </div>
       </div>
       <Outlet />
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -4,11 +4,15 @@ const uiSlice = createSlice({
   name: "ui",
   initialState: {
     loading: false,
+    theme: "light",
     //notification: null,
   },
   reducers: {
     load(state, action) {
       state.loading = action.payload;
+    },
+    toggleTheme(state) {
+      state.theme = state.theme === "light" ? "dark" : "light";
     },
     // showNotification(state, action) {
     //   const { status, title, message } = action.payload;
